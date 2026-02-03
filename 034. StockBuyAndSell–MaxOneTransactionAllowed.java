016e344b7c8b156ed6baee1efe333452c8e80408
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-02-03 13:17:07 +0530

    3 February

---
+++ 034. StockBuyAndSell–MaxOneTransactionAllowed.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int n = prices.length;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        for(int i=0; i<n; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        
+        return maxProfit;
+    }
+}
