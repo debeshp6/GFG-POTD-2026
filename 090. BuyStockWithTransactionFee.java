f00ab423183ec4884883109cf804aa3d9a3805e0
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-03-31 07:30:42 +0530

    31 March

---
+++ 090. BuyStockWithTransactionFee.java
@@ -0,0 +1,19 @@
+
+
+
+class Solution {
+    public int maxProfit(int[] arr, int k) {
+        int n = arr.length;
+        if (n == 0) return 0;
+
+        int hold = -arr[0];  
+        int cash = 0;        
+        
+        for (int i = 1; i < n; i++) {
+            hold = Math.max(hold, cash - arr[i]);
+            cash = Math.max(cash, hold + arr[i] - k);
+        }
+        
+        return cash; 
+    }
+}
