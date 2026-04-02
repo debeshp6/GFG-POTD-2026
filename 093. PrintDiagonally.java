16375525cfc3b1be224e53a36d667817cc3ec5cb
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-04-03 01:02:30 +0530

    3 April

---
+++ 093. PrintDiagonally.java
@@ -0,0 +1,23 @@
+class Solution {
+    static ArrayList<Integer> diagView(int mat[][]) {
+        int n = mat.length;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int sum = 0; sum <= 2 * (n - 1); sum++) {
+
+            int row = Math.max(0, sum - n + 1);
+            int col = sum - row;
+            while (row < n && col >= 0) {
+                result.add(mat[row][col]);
+                row++;
+                col--;
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+
