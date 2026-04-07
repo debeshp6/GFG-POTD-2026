7b44dbde32c7f79a060733de6b37217d1135efd7
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-04-08 01:01:55 +0530

    8 April

---
+++ 098. Segregate0sAnd1s.java
@@ -0,0 +1,13 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        
+        int index=0;
+        
+        for(int i=0; i<arr.length; ++i) {
+            if(arr[i]==0) arr[index++]=arr[i];
+            
+        }
+        
+        for(int i=index; i<arr.length; ++i) arr[i]=1;
+    }
+}
