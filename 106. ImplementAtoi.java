4317160e3eb1c7e5dcae79d8f6064ce254153cf8
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-04-16 11:44:18 +0530

    16 April

---
+++ 106. ImplementAtoi.java
@@ -0,0 +1,33 @@
+
+
+class Solution {
+    public int myAtoi(String s) {
+        
+        s = s.trim();
+        if (s.isEmpty()) return 0;
+        int sign = 1, i = 0;
+        if (s.charAt(0) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(0) == '+') {
+            i++;
+        }
+
+        long result = 0;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            if (result * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (result * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+        
+        return (int) (result * sign);
+    }
+}
+
+
+
+
+
+
+
