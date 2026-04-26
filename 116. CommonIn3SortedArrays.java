2a7e035a44dc4e10fb0d50a582dc54c140d73b03
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2026-04-26 21:20:09 +0530

    26 April

---
+++ 116. CommonIn3SortedArrays.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+       
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        
+        while (i < a.length && j < b.length && k < c.length) {
+            if (a[i] == b[j] && b[j] == c[k]) {
+                if((ans.size() > 0 && ans.get(ans.size()-1) != a[i] ) || ans.size() == 0)
+                ans.add(a[i]);
+                i++;
+                j++;
+                k++;
+            } 
+            
+            if(i >= a.length || j >= b.length || k >= c.length) break;
+            int max = Math.max(a[i], Math.max(b[j], c[k]));
+            
+            while(i < a.length && a[i] < max) i++;
+            while(j < b.length && b[j] < max) j++;
+            while(k < c.length && c[k] < max) k++;
+            
+            
+        }
+        
+        return ans;
+    }
+}
